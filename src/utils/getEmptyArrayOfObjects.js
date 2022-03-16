440d1abb8874a4959d6ddaa651257f0669b7d119
BalajiHarinath <89400019+BalajiHarinath@users.noreply.github.com>
2022-03-16 16:55:44 +0530

    refacor : removed an extra file

--- src/utils/getEmptyArrayOfObjects.js
+++
@@ -1,6 +0,0 @@
-import { v4 as uuid } from "uuid";
-
-export const getEmptyArrayOfObjects = (arrayLength, data) => {
-    console.log(data);
-    return new Array(arrayLength).fill(data).map((_) => ({ _id: uuid() }));
-  };
