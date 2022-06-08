425b44ef6654df7cacbd9f53e201dd36f9664a9e
BalajiHarinath <89400019+BalajiHarinath@users.noreply.github.com>
2022-06-08 18:56:00 +0530

    refactor: Changed the user database

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarsh123",
+    firstName: "Balaji",
+    lastName: "Harinath",
+    email: "balaji@gmail.com",
+    password: "balaji12345",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
